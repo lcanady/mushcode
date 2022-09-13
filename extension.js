bf90b64d8b72617884e900bb0237e362b6abc5b6
Lem Canady <lem.canady@gmail.com>
2022-09-13 15:19:16 -0700

    added Rhost API support

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 const vscode = require("vscode");
 const { formatter } = require("@digibear/mush-format");
+const { default: axios } = require("axios");
 
 function activate(context) {
   let disposable = vscode.commands.registerCommand(
@@ -18,7 +19,45 @@ function activate(context) {
     }
   );
 
-  context.subscriptions.push(disposable);
+  let disposable2 = vscode.commands.registerCommand(
+    "extension.postSelection",
+    async function () {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        console.log("No editor!");
+        return;
+      }
+      const document = editor.document;
+      const selection = editor.selection;
+      const text = document.getText(selection);
+      const formatted = await formatter.format(text);
+      const user = vscode.workspace.getConfiguration("game").get("dbref");
+      const pass = vscode.workspace.getConfiguration("game").get("password");
+      const host = vscode.workspace.getConfiguration("game").get("host");
+      const port = vscode.workspace.getConfiguration("game").get("port");
+      const buff = new Buffer.from(
+        formatted.data.replace(/([\[\]%\{\};])/g, "%$1").replace(/\\/g, "%\\")
+      );
+      const res = await axios({
+        method: "post",
+        url: `${host}:${port}`,
+        timeout: 2000,
+        auth: {
+          username: user,
+          password: pass,
+        },
+        headers: {
+          exec64: buff.toString("base64"),
+        },
+      }).catch((err) => console.log(err.message));
+
+      if (res && res.status == 200) {
+        vscode.window.showInformationMessage("Selection Posted to game!");
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable, disposable2);
 }
 
 exports.activate = activate;
